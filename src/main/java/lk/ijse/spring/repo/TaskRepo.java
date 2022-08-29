20142857a3f9aec454074e491ddbaf9635c64eda
parindyawijeseskara <parindyawijesekara@gmail.com>
2022-08-29 17:16:54 +0530

    Added UserRepo and TaskRepo

--- src/main/java/lk/ijse/spring/repo/TaskRepo.java
+++ src/main/java/lk/ijse/spring/repo/TaskRepo.java
@@ -1,4 +1,4 @@
 package lk.ijse.spring.repo;
 
-public class TaskRepo {
+public interface TaskRepo {
 }
--- src/main/java/lk/ijse/spring/repo/UserRepo.java
+++ src/main/java/lk/ijse/spring/repo/UserRepo.java
@@ -1,4 +1,4 @@
 package lk.ijse.spring.repo;
 
-public class UserRepo {
+public interface UserRepo {
 }
